b24b4dd5949fa17c59393bfb69bbeee20c18a225
Fred <akamaatei@gmail.com>
2020-10-26 15:49:09 +0300

    add login

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from flask_login import login_required
+from ..models import Reviews, User
 
 
 
@@ -15,3 +16,12 @@ def new_pitch(id):
     title = 'Pitch Deck'
 
     return render_template('index.html', title = title) 
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
