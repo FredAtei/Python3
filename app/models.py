8c94e2bb97227dd90fec0f71d0dd163eaf617c66
Fred <akamaatei@gmail.com>
2020-10-26 14:33:46 +0300

    modify files for page

--- app/models.py
+++ app/models.py
@@ -2,8 +2,11 @@ from . import db
 
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
 
     def __repr__(self):
         return f'User {self.username}'
@@ -22,24 +25,15 @@ class Pitch(db.Model):
         
     def __repr__(self):
         return f'Pitch {self.post}'
+        
 
-class Comment(db.Model):
-    __tablename__ = 'comments'
-    id = db.Column(db.Integer, primary_key=True)
-    comment = db.Column(db.Text(),nullable = False)
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'),nullable = False)
+class Role(db.Model):
+    __tablename__ = 'roles'
 
-    def save_c(self):
-        db.session.add(self)
-        db.session.commit()
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
 
-    @classmethod
-    def get_comments(cls,pitch_id):
-        comments = Comment.query.filter_by(pitch_id=pitch_id).all()
 
-        return comments
-
-    
     def __repr__(self):
-        return f'comment:{self.comment}'        
+        return f'User {self.name}'       
