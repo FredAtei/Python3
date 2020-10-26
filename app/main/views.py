6daa4e9822d2c9ee463bab84f22889d7a1d85efe
Fred <akamaatei@gmail.com>
2020-10-26 15:17:54 +0300

    add update on login

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,12 @@
 from flask import render_template,request,redirect,url_for
 from . import main
+from flask_login import login_required
 
 
 
-@main.route('/')
-def index():
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_pitch(id):
 
     '''
     View root page function that returns the index page and its data
