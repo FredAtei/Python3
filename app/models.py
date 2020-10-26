94af3ad0b680d5451cfca51d1787a85f405ffcc6
Fred <akamaatei@gmail.com>
2020-10-26 15:27:09 +0300

    add more models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,12 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Pitch(db.Model):
     __tablename__ = 'pitches'
@@ -26,11 +33,12 @@ class Role(db.Model):
     def __repr__(self):
         return f'User {self.name}'
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
 
